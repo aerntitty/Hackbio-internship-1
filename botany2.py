ea97f803a7faddfd264aa53ef33816eb5b4c3b57
aerntitty <niyeh24@gmail.com>
2025-02-23 02:03:08 +0100

    botany

---
+++ botany2.py
@@ -0,0 +1,112 @@
+"""
+
+Task Code 2.3:
+Botany and Plant Science
+Have a look at this dataset
+Some scientists are trying to engineer mutants for a crop to become resistant to a pesticide
+They compared the metabolic response of the engineered mutants to the metabolic response of the wild type plants
+The took readings after 8 and 24 hours
+Your task
+Calculate the difference in metabolic response (ΔM) between the DMSO treatment from the 24 hours treatment for the wild type and mutants
+Generate a scatter plot showing the difference for ΔM for WT and Mutants
+Fit a line that satifies a y-intercept of 0 and a slope of 1.
+Using a residual cut off of your choice (calculated a the difference between the fitted line and each point) calculate the residual of each point on the scatter plot
+Color metabolites that fall within +/- n of your residual grey. For example, if you have a cut-off of 0.3, color residual values that are within -0.3 and +0.3 grey
+Color metabolites that fall outside this range salmon.
+What are these metabolites. How do you explain the trends you see on either direction of the plot?
+Pick any 6 metabolites that fall outside this range and generate a line plot that spans from their 0h treatment to their 8h and 24hr.
+What can you say about the plots you see?
+
+"""
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df= pd.read_csv(r"Stage 2/Pesticide_treatment_data.csv",sep=',', index_col=0)
+print(df.head(9))
+
+# difference in metabolic response (ΔM) between the DMSO treatment from the 24 hours treatment for the wild type and mutants
+# Select relevant rows for wild_type
+
+WT_dmso=df.loc['WT_DMSO_1']
+WT_24h =df.loc["WT_pesticide_24h_1"]
+
+# select relevan roles for the mutant
+
+mutant_DMSO =df.loc["mutant_DMSO_1"]
+mutant_24hrs= df.loc["mutant_pesticide_24h_1"]
+
+
+# Compute ΔM for each condition
+deltaM_wt=WT_24h - WT_dmso
+deltaM_mut=mutant_24hrs-mutant_DMSO
+
+
+#create a dataframe
+change_df=pd.DataFrame({'wt_change':deltaM_wt, 'mut_change':deltaM_mut})
+print(change_df.head())
+
+
+#create residual 
+
+
+# Define cutoff
+cutoff = 0.2
+
+# Compute residuals
+change_df["residual"]=change_df['mut_change']- change_df['wt_change']
+print(change_df.head())
+
+
+# Assign colors based on cutoff
+def get_colour(residual):
+    if -cutoff<= residual <=cutoff:
+        return'grey'
+    return 'pink'
+
+
+# Apply function to get colors
+colors= change_df['residual'].apply(get_colour)
+print(change_df.head())
+
+
+# Identify outlier metabolites
+outliers= change_df.loc[change_df['residual'].abs()>cutoff].index.to_list()
+print(outliers)
+
+
+# Generate a scatter plot showing the difference for ΔM for WT and Mutants using the colors as hues
+plt.figure(figsize=(10,6))
+sns.scatterplot(x='wt_change',y='mut_change',data=change_df,hue=colors,palette={"grey":"grey", "pink":"pink"})
+
+
+# Fit a y = x reference line (slope=1, intercept=0)
+x_vals=change_df['wt_change']
+plt.plot(x_vals,x_vals, color='cyan',linestyle="--",label='y=x=(slope=1)')
+
+plt.xlabel('wt_change')
+plt.ylabel('mut_change')
+plt.title("scatter_plot of wt/mt")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+# Generate line plots for outlier metabolite
+#i will select 6 metabolites
+selcted_cols=[ 'glycine','hexose_6_phosphate','isoleucine','pyruvic_acid','adipic_acid','uracil']
+
+# Plot line plots for outlier metabolites
+for metabolite in selcted_cols:
+    plt.figure(figsize=(16,6))
+    plt.plot(df.index,df[metabolite],color="pink",marker='o',linestyle='--')
+    plt.xlabel('time_Conditions')
+    plt.ylabel('metabolic_respnse')
+    plt.xticks(rotation=50)
+    plt.title(f'line plot for {metabolite}')
+    plt.legend()
+    plt.show()
